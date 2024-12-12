8fd9ed7f57b626b11a5c0d1fe543eeba9951feb5
YamauchiKensei <kensei1110@icloud.com>
2024-12-12 18:43:11 +0900

    model内の結合項を先に計算した

--- model.m
+++ model.m
@@ -1,16 +1,15 @@
 function dydt = model(~, y, alpha, omega, K, A)
-    N = length(y) / 2; % yの長さを2で割ってノード数を取得
+    N = length(y) / 2; % ノード数を取得
     z = y(1:N) + 1i * y(N+1:end); % 実部と虚部を複素数に変換
 
-    dzdt = zeros(N, 1) + 1i * zeros(N, 1);
-    for j = 1:N
-        dzdt(j) = (alpha(j) + 1i * omega - abs(z(j))^2) * z(j);
-        for k = 1:N
-            dzdt(j) = dzdt(j) + (K / N) * A(j, k) * (z(k) - z(j));
-        end
-    end
+    % 結合項の計算
+    coupling = (K / N) * (A * z - z .* sum(A, 2)); % zの結合項を行列演算で計算
 
-    % dzdtを実部と虚部に分けて返す
+    % dz/dtの計算
+    dzdt = (alpha + 1i * omega - abs(z).^2) .* z + coupling;
+
+    % 実部と虚部に分けて返す
     dydt = [real(dzdt); imag(dzdt)];
 end
+
 
